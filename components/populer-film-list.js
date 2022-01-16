ef2e5d1c41a439497009e465b3792796daf288fa
Göktuğ Mustafa Sultan <goktugsultan@gmail.com>
2022-01-16 23:21:57 +0300

    update popular film list

--- components/populer-film-list.js
+++ components/populer-film-list.js
@@ -1,10 +1,57 @@
-export default function PopularFilm({ movies}) {
-    return <div>
+export default function PopularFilm({ movies }) {
+    return <div className="popular-films">
         {movies.results.map(movie => (
-            <div key={movie.title}>
-                <p>{movie.title}</p>
+            <div className="popular-film-item" key={movie.title}>
                 <img src={`https://www.themoviedb.org/t/p/w220_and_h330_face${movie.poster_path}`} alt="" />
+                <div className="movie-details">
+                    <p>{movie.title}</p>
+                    <span>{movie.overview}</span>
+                </div>
+
+
             </div>
         ))}
+        <style jsx>{`
+        .popular-films{
+            display:flex;
+            flex-direction:column;
+            width:80%;
+            justify-content:center;
+            align-items:center;
+            margin-left:auto;
+            margin-right:auto;
+        }
+        .popular-film-item{
+            display:flex;
+            width:100%;
+            padding:20px 20px;
+            border:1px solid #efefef;
+            margin-bottom:8px;
+        }
+        .movie-details{
+            padding-left:20px;
+            p{
+                font-size:24px;
+                margin-bottom:8px;
+                border-bottom:1px solid #ccc;
+            }
+            span{
+                line-height:1.5;
+            }
+        }
+        @media screen and (max-width: 600px) {
+            .popular-film-item{
+              flex-direction:column;
+            }
+            .movie-details{
+              padding-left:0;  
+               p{
+                 margin-top:18px;   
+               }
+            }
+          }
+        
+        `}
+        </style>
     </div>
 }
